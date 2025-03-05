37745c837738854ead689df5a95d413621935c21
adrian161103 <adriankeomaa@gmail.com>
2025-03-04 22:42:50 -0300

    new chamges

--- main.jsx
+++ main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "../src/style/index.css";
+import "./src/style/index.css";
 import { RouterProvider } from "react-router-dom";
-import { router } from "./router";
+import { router } from "./src/router.jsx";
 
 
 createRoot(document.getElementById('root')).render(
