6d28e9535d19b95d6bd16b8251c7bc1437748785
adrian161103 <adriankeomaa@gmail.com>
2025-03-08 23:23:26 -0300

    new change image and particles

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,17 +1,54 @@
-
-import useNeonParticles   from '../hooks/particles.jsx'
+import React, { useEffect, useRef } from 'react';
+import useNeonParticles from '../hooks/particles.jsx';
 import Particles from "react-tsparticles";
-function image() {
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-  const { particlesInit, particlesLoaded, options } = useNeonParticles()
+
+function Image() {
+  const { particlesInit, particlesLoaded, options } = useNeonParticles();
+  const bgRef = useRef(null);
+
+  useEffect(() => {
+    const updateBgHeight = () => {
+      if (bgRef.current && window.visualViewport) {
+        bgRef.current.style.height = `${window.visualViewport.height}px`;
+      } else if (bgRef.current) {
+        bgRef.current.style.height = `${window.innerHeight}px`;
+      }
+    };
+
+    updateBgHeight();
+
+    if (window.visualViewport) {
+      window.visualViewport.addEventListener('resize', updateBgHeight);
+    } else {
+      window.addEventListener('resize', updateBgHeight);
+    }
+    return () => {
+      if (window.visualViewport) {
+        window.visualViewport.removeEventListener('resize', updateBgHeight);
+      } else {
+        window.removeEventListener('resize', updateBgHeight);
+      }
+    };
+  }, []);
+
   return (
     <>
-    <Particles  init={particlesInit}
-            loaded={particlesLoaded}
-            options={options} />
-      <img className="Image"  src="img/cyber.jpg" alt="CyberPunk" draggable="false" style={{ userSelect: "none", pointerEvents: "none" }}/>
+      <Particles
+        init={particlesInit}
+        loaded={particlesLoaded}
+        options={options}
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100vh",
+          zIndex: 0,
+        }}
+      />
+      <div ref={bgRef} className="Image"></div>
     </>
   );
 }
 
-export default image;
+export default Image;
